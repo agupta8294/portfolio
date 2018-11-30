37b8639053833662fc1216f3e840398f917a2468
Anurag Gupta <anurag.2005ca@gmail.com>
2018-11-30 03:57:13 +0000

    dis again

--- bootstrap-4.1.2-dist/js/bootstrap.js
+++ bootstrap-4.1.2-dist/js/bootstrap.js
@@ -21,6 +21,7 @@
       Object.defineProperty(target, descriptor.key, descriptor);
     }
   }
+  
 
   function _createClass(Constructor, protoProps, staticProps) {
     if (protoProps) _defineProperties(Constructor.prototype, protoProps);
